871be984e12a394bed76652fba71566c9a4dde8c
anton <sims0204@gmail.com>
2021-04-14 11:10:58 +0400

    hw7

--- js2-7/public/script.js
+++ js2-7/public/script.js
@@ -1,19 +1,19 @@
-const API_URL = '/goods.json';
 
 
-Vue.component('search-good', { //строка поиска
-  template: '<input v-model="search" v-on:input="searchGood"></input>',
+Vue.component('search', {
+  template: '<div><input id="search" v-model="search"><button v-on:click="searchHandler">SEARCH</button></div>',
   data() {
     return {
-      search: ''
+      search: '',
     }
   },
   methods: {
-    searchGood() {
-      this.$emit('searching', this.search)
+    searchHandler() {
+      this.$emit('search', this.search);
     }
   }
 })
+
 Vue.component('goods-item', { // Создание нового компонента
   template: '<div :data-id="id" class="goods-item"><h3>{{ title }}</h3><p>{{ price }}</p></div>',
   props: ['title', 'price', 'id'] // задаем параметры компонента
@@ -41,84 +41,99 @@ Vue.component('cart', { // создание компонента корзины
     }
   }
 })
-Vue.component('not-connect', {
-  template: '<div><h1>Все пропало</h1></div>',
 
-})
 const vue = new Vue({
   el: "#app",
   data: {
     cart: [],
     goods: [],
     filtredGoods: [],
-    isNotConnect: 0
-    //search: ''
+    search: '',
+    isLoaded: false,
   },
   methods: {
-    showFault(err) {
-      this.isNotConnect = err;
+    addStats(data) {
+      //действие, название товара, время 
+      fetch('/stats', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      });
     },
     addToCartHandler(e) {
       const id = e.target.closest('.goods-item').dataset.id;
       const good = this.goods.find((item) => item.id == id);
 
+      fetch('/cart', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(good)
+      });
+
+      this.addStats({
+        move: 'add',
+        name: good.title,
+        time: e.timeStamp
+      });
+
       this.cart.push(good);
     },
 
     removeFromCartHandler(e) {
-      console.log(e)
       const id = e.target.closest('.goods-item').dataset.id;
       const goodIndex = this.cart.findIndex((item) => item.id == id);
+      const good = this.goods.find((item) => item.id == id);
+
+      fetch('/cart_splice', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ index: goodIndex })
+      });
+
+      this.addStats({
+        move: 'remove',
+        name: good.title,
+        time: e.timeStamp
+      });
 
       this.cart.splice(goodIndex, 1);
     },
 
-    searchHandler(data) {
-      if (data === '') {
+    searchHandler(search) {
+      if (search === '') {
         this.filtredGoods = this.goods;
       }
-      const regexp = new RegExp(data, 'gi');
+      const regexp = new RegExp(search, 'gi');
       this.filtredGoods = this.goods.filter((good) => regexp.test(good.title));
     },
 
-    fetch(error, success) {
-      let xhr;
-
-      if (window.XMLHttpRequest) {
-        xhr = new XMLHttpRequest();
-      } else if (window.ActiveXObject) {
-        xhr = new ActiveXObject("Microsoft.XMLHTTP");
-      }
-
-      xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4) {
-          if (xhr.status === 200) {
-            success(JSON.parse(xhr.responseText));
-          } else if (xhr.status > 400) {
-            error(true);
-          }
-        }
-      }
-
-      xhr.open('GET', API_URL, true);
-      xhr.send();
-    },
-
-    fetchPromise() {
-      return new Promise((resolve, reject) => {
-        this.fetch(reject, resolve)
-      })
-    }
   },
   mounted() {
-    this.fetchPromise()
+    fetch('/data')
+      .then(response => response.json())
       .then(data => {
         this.goods = data;
         this.filtredGoods = data;
+
+        this.isLoaded = true;
       })
       .catch(err => {
-        this.isNotConnect = err;
-        console.log(err)
+        console.log(err);
+      })
+
+    fetch('/cart')
+      .then(response => response.json())
+      .then(data => {
+        this.cart = data;
+      })
+      .catch(err => {
+        console.log(err);
       })
   }
 })
